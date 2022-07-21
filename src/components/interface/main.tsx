d8c498efc194eb9c5772e697ea71aead6c8ec8c9
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-21 19:54:51 +0200

    Initial commit

--- src/components/interface/main.tsx
+++ src/components/interface/main.tsx
@@ -9,17 +9,10 @@ import {PointCollection} from "../../core/point-collection";
 export function Container()
 {
     return (
-        <div className={"w-screen h-screen space-x-4"}>
-            <OpenStreetMap className={"map m-3.5 w-3/5"}>
-                <Land pointCollection={new PointCollection([[30, 45]])}/>
+        <div className={"w-screen h-screen flex "}>
+            <OpenStreetMap style={{height: "100%"}} className={"border-2 w-2/3"}>
+                <Land pointCollection={new PointCollection([[20, 30]])}/>
             </OpenStreetMap>
-            <div className={"interface m-3.5 border-2 flex basis-1 grow"}>
-                border
-            </div>
-            <div className={"interface m-3.5 border-2 flex basis-1 grow-2"}>
-                border
-            </div>
-
         </div>
     )
 }
